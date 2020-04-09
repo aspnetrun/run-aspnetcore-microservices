f67c6803931159310c9f675de7e73635a3c063a6
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-04-09 17:42:28 +0300

    Product added

---
+++ src/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }        
+    }
+}
