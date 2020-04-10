33b7c8a63bb58d81c8009fa525a151040ee34441
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-04-10 12:31:38 +0300

    swagger added

--- src/Catalog/Catalog.API/Startup.cs
+++ src/Catalog/Catalog.API/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace Catalog.API
 {
@@ -49,6 +50,15 @@ namespace Catalog.API
 
             #endregion
 
+            #region Swagger Dependencies
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog API", Version = "v1" });
+            });
+
+            #endregion
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +77,13 @@ namespace Catalog.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API V1");
+            });
+
         }
     }
 }
