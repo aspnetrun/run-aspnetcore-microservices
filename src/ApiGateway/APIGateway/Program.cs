0a43e1a31453e9361205dfca006c1939c5aac597
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-04-16 20:02:39 +0300

    ocelot api gateway dokerized

--- src/ApiGateway/APIGateway/Program.cs
+++ src/ApiGateway/APIGateway/Program.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace APIGateway
-{
+{    
     public class Program
     {
         public static void Main(string[] args)
@@ -24,7 +19,11 @@ namespace APIGateway
                 })
             .ConfigureAppConfiguration((hostingContext, config) =>
             {
-                config.AddJsonFile("ocelot.json");
+                config.AddJsonFile($"ocelot.json");
+                
+                // for local test use below one, multi env json file not worked
+                //config.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+                // https://github.com/ThreeMammals/Ocelot/issues/249
             });
     }
 }
