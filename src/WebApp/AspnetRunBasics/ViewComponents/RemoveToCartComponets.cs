4f1baf0068503ce90b6e1870000e98349d5573e6
Sedat Girgin <sedat_girgin@yanmar-turkiye.com>
2021-02-20 14:40:51 +0300

    changed

---
+++ src/WebApp/AspnetRunBasics/ViewComponents/RemoveToCartComponets.cs
@@ -0,0 +1,23 @@
+﻿using AspnetRunBasics.ApiCollection.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ViewComponents
+{
+    public class RemoveToCartComponets : ViewComponent
+    {
+        private readonly IBasketApi _basketApi;
+
+        public RemoveToCartComponets(IBasketApi basketApi, ICatalogApi catalogApi)
+        {
+            _basketApi = basketApi ?? throw new ArgumentNullException(nameof(basketApi));
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("_RemoteToCartPartial", await _basketApi.GetBasket("swn"));
+        }
+    }
+}
