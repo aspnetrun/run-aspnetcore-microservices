a613a2846269030e61b03682fb56773b4d172983
tajoutihamza <46810740+tajoutihamza@users.noreply.github.com>
2022-12-09 14:29:06 +0100

    the data seed happen at the start of the application not at the creation of CatalogContext

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -13,7 +13,6 @@ namespace Catalog.API.Data
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
-            CatalogContextSeed.SeedData(Products);
         }
 
         public IMongoCollection<Product> Products { get; }
