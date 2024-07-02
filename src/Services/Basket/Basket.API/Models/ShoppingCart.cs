68bdc8d728a1c96bfda17178693620fd02bc5f53
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-07-02 17:54:41 +0400

    Refactor code

--- src/Services/Basket/Basket.API/Models/ShoppingCart.cs
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -6,10 +6,7 @@ public class ShoppingCart
     public List<ShoppingCartItem> Items { get; set; } = new();
     public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
 
-    public ShoppingCart(string userName)
-    {
-        UserName = userName;
-    }
+    public ShoppingCart(string userName) => UserName = userName;
 
     //Required for Mapping
     public ShoppingCart()
--- src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -5,10 +5,7 @@ public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
     private readonly List<IDomainEvent> _domainEvents = new();
     public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
 
-    public void AddDomainEvent(IDomainEvent domainEvent)
-    {
-        _domainEvents.Add(domainEvent);
-    }
+    public void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
 
     public IDomainEvent[] ClearDomainEvents()
     {
