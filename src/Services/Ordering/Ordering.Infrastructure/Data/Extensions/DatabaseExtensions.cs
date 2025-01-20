7218e56e513892b5d94d95355f6ae7c285404dcd
Ivan Smith <senja2018@mail.ru>
2025-01-20 23:42:15 +0300

    Исправлены ошибки в названии файлов

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -2,12 +2,12 @@
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Infrastructure.Data.Extensions;
-public static class DatabaseExtentions
+public static class DatabaseExtensions
 {
     public static async Task InitialiseDatabaseAsync(this WebApplication app)
     {
         using var scope = app.Services.CreateScope();
-
+        
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         context.Database.MigrateAsync().GetAwaiter().GetResult();
